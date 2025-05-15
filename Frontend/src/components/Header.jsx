2e0293dfef905bfe65deca7286df1404c2bf007e
naveen-sen <naveennapit@gmail.com>
2025-05-15 21:07:22 +0530

    New Project

---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,86 @@
+import { Button } from '@/components/ui/button'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
+import { ChevronDown, FileText, GraduationCap, LayoutDashboard, PenBox, StarsIcon } from 'lucide-react'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+
+function Header() {
+  return (
+    <header className='fixed top-0 w-[80vw] border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60'>
+      <nav className='container mx-auto px-4 h-18 flex items-center justify-between'>
+        
+        <div className='flex items-start justify-start'>
+          <a href='/'>
+            <img src='/logo.png' alt='Logo' className='h-10 w-auto' />
+          </a>
+        </div>
+        
+
+        <div className='flex space-x-4 justify-end items-end'>
+          <SignedIn>
+            <a href='/dashboard'>
+              <Button className='flex items-center'>
+                <LayoutDashboard className='w-5 h-5' />
+                <span className='hidden md:block '>Industry Insights</span>
+              </Button>
+            </a>
+          
+
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Button className='flex items-center'>
+                <StarsIcon className='w-5 h-5 mr-2' />
+                <span className='hidden md:block '>Growth Tools</span>
+                <ChevronDown className='w-4 h-4 ml-2' />	
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>
+              <Button className='flex items-center'>
+                <FileText className='w-5 h-5 mr-2' />
+                <span className='hidden md:block '>Build Resume</span>
+              </Button>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Button className='flex items-center'>
+                <PenBox className='w-5 h-5 mr-2' />
+                <span className='hidden md:block '>Cover Letter</span>
+              </Button>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Button className='flex items-center'>
+                <GraduationCap className='w-5 h-5 mr-2' />
+                <span className='hidden md:block '>Interview Prep</span>
+              </Button>
+            </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton>
+              <Button>Sign In</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton
+              afterSignOutUrl='/'
+              appearance={{
+                elements: {
+                  avatarBox: 'h-10 w-10',
+                  userButtonAvatar: 'h-10 w-10 rounded-full',
+                  userButtonPopoverCard: 'w-[200px] shadow-xl',
+                  userButtonPopoverAction: 'w-full text-left',
+                  userPreviewMainIdentifier: 'font-semibold'
+                },
+              }}
+            />
+          </SignedIn>
+
+        </div>
+          
+      </nav>
+    </header>
+  )
+}
+
+export default Header
---
+++ Frontend/src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { ThemeProvider } from '@/components/ui/theme-provider'
+import { dark } from '@clerk/themes'
+import {ClerkProvider} from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
+createRoot(document.getElementById('root')).render(
+  
+    <StrictMode>
+      <ThemeProvider>
+        <ClerkProvider
+        appearance={{
+        baseTheme: dark,
+        }}
+        publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+          <App />
+        </ClerkProvider>
+        
+      </ThemeProvider>
+    </StrictMode>
+  
+  
+  
+)
