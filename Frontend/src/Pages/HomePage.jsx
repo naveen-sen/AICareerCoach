a439560c719abd864cd59c6d3488bb7b00e38da1
naveen-sen <naveennapit@gmail.com>
2025-06-20 16:16:34 +0530

    Update email link in HomePage

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -115,7 +115,7 @@ function HomePage() {
           </div>
 
           <div className="flex justify-center gap-6 mt-6 text-muted-foreground">
-            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=naveennapit@gmail.com" target="_blank" rel="noopener noreferrer">
+            <a href="mailto:naveennapit@gmail.com" target="_blank" rel="noopener noreferrer">
               <svg className='w-6 h-6' fill="currentColor" viewBox="0 0 16 16">
                 <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
               </svg>
